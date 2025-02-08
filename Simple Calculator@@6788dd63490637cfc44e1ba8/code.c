ec728eb11a8c18fd349b32881ec124e8225b78ea
vanshika-1707 <vanshika4mittal@gmail.com>
2025-02-08 15:45:57 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 int main(){
     int num1, num2;
     char operator;
     scanf("%d %d", &num1, &num2);
-    scanf("%c", &operator);
+    scanf("%c", &operator); // Corrected line
+
     if (operator == '+'){
         printf("%d + %d = %d\n", num1, num2, num1 + num2);
     }
